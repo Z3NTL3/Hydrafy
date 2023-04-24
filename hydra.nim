775f6a131e89165ec18f286149330e10ea657e4e
z3ntl3 <48758770+Z3NTL3@users.noreply.github.com>
2023-04-24 14:27:01 +0200

    Update hydra.nim

--- hydra.nim
+++ hydra.nim
@@ -30,7 +30,7 @@ proc Usage(): void =
     $terminal["red"] & "Invalid usage" &
     $terminal["reset"]
   echo $terminal["bold"] &
-    "Usage: <bin> --bytes:<number> --host:<target> --port:<port> --timeoutMS:<timeout>" &
+    "Usage: myprogram --bytes:<number> --host:<target> --port:<port> --timeoutMS:<timeout>" &
     $terminal["reset"]
 
 var retrievedOptsCount = 0
@@ -68,9 +68,14 @@ proc stress_test(host: string, port: int, use: bool, size: int): void {.thread.}
     var s = newSocket(AF_INET, SOCK_STREAM, IPPROTO_TCP)
 
     if(use): s.bindAddr(address=host, port=Port(port))
+    s.connect(
+      host,
+      Port(port), 
+      timeoutMS
+    )
 
     var payload = urandom(size)
-    s.sendTo(host,Port(port), addr(payload), payload.len)
+    discard s.send(data=addr(payload), payload.len)
 
     echo "SEND " & $payload.len & " bytes amount to " & host & ":" & $port
     s.close()
